b89f13087b39275a2e87e7dc35db6c438b466b4c
Shashwat8732 <Shashwatraj8732@gmail.com>
2025-12-24 14:07:28 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -10,7 +10,7 @@ app = FastAPI(title="CyberSec Assistant API")
 agent=ReAct_Agent()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://victorious-grass-0e4662f00.2.azurestaticapps.net/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
